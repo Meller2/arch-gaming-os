d9e1e1fb25913153874380c6aec4e6e7912d875a
Meller2 <diyarov983@gmail.com>
2026-05-22 21:48:13 +0300

    fix: create liveuser for SDDM autologin and desktop access
    Without a liveuser account, SDDM autologin fails and the user is
    locked out of the desktop. Only root existed with no way to log in
    through the display manager.
    
    Changes:
    - Add /etc/passwd and /etc/shadow overlay with liveuser (UID 1000)
      so mkarchiso copies /etc/skel/ to the home directory
    - Update customize_airootfs.sh to:
      * Create liveuser with groups: wheel, autologin, audio, video,
        optical, storage, games, power
      * Set empty password for liveuser (passwd -d)
      * Enable wheel group in sudoers for passwordless sudo

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -10,3 +10,11 @@ COMPRESSION_OPTIONS=(-9e)
 MKINITCPIOEOF
 
 mkinitcpio -p linux-zen
+
+useradd -m -G wheel,autologin,audio,video,optical,storage,games,power -s /usr/bin/bash liveuser
+passwd -d liveuser
+
+groupadd -r autologin
+gpasswd -a liveuser autologin
+
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
