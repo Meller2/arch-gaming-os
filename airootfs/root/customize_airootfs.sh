74bef3395f89a82b292393f66f871c184fb619c9
Meller2 <diyarov983@gmail.com>
2026-05-23 15:56:58 +0300

    fix: expired password blocks xdg-user-dirs-update in chroot
    chage sets password to never expire, su replaces sudo to avoid
    password prompt in CI container

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -14,9 +14,10 @@ mkinitcpio -p linux-zen
 groupadd -r autologin
 useradd -m -G wheel,autologin,audio,video,optical,storage,games,power -s /usr/bin/bash liveuser
 echo "liveuser:liveuser" | chpasswd
+chage -d -1 -M -1 liveuser
 
 sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 
 systemctl enable sshd NetworkManager avahi-daemon bluetooth
 
-sudo -u liveuser xdg-user-dirs-update
+su - liveuser -c "xdg-user-dirs-update"
